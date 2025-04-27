e4885e12a6b51ff546cea4dcb7a4632b1503d141
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-04-27 23:49:11 +0900

    modified design

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1 class="text-center text-5xl text-red-600">Manchester United Information</h1>
+    <h1 class="text-center text-5xl text-red-600">Manchester is Red</h1>
   </div>
 </template>
