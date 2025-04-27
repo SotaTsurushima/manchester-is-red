95870bcbf0dab9a5a0409f18647de3a0484f88cb
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-04-27 23:40:20 +0900

    simplified first page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,36 +1,5 @@
 <template>
   <div>
-    <h1>Manchester United Information</h1>
-    <div v-if="teamData">
-      <p>Name: {{ teamData.name }}</p>
-      <p>Short Name: {{ teamData.shortName }}</p>
-      <p>Founded: {{ teamData.founded }}</p>
-      <p>Stadium: {{ teamData.stadium }}</p>
-      <p>Country: {{ teamData.country }}</p>
-    </div>
-    <div v-else>
-      <p>Loading...</p>
-    </div>
+    <h1 class="text-center text-5xl text-red-600">Manchester United Information</h1>
   </div>
 </template>
-
-<script setup>
-import { ref, onMounted } from 'vue'
-
-const teamData = ref(null)
-
-onMounted(async () => {
-  // const response = await fetch('http://localhost:8000/manchester_united')
-  // if (response.ok) {
-  //   teamData.value = await response.json()
-  // } else {
-  //   console.error('Failed to fetch data')
-  // }
-})
-</script>
-
-<style scoped>
-h1 {
-  color: #c8102e; /* Manchester United's red */
-}
-</style>
