53cb5ab327a8a5692f11e228118440e96d60f241
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-04-28 00:36:07 +0900

    modified for sppeding up the startup time

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,6 +6,9 @@ export default defineNuxtConfig({
         usePolling: true, // ホットリロードが反応しない場合は polling を有効にする
       },
     },
+    optimizeDeps: {
+      exclude: ['some-heavy-dependency'],  // 不要な依存関係を除外してビルドを軽くする
+    },
   },
   modules: [
     '@nuxtjs/tailwindcss', // ← 追加するだけ！
