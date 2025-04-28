70f81cfdc56e429efafaf014f8aeafe9e9ba56e7
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-04-28 18:47:55 +0900

    mofdified navigation

--- frontend/components/Navigation.vue
+++ frontend/components/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="bg-red-700 text-white p-3 flex justify-between items-center font-sans">
+  <nav class="bg-red-700 text-white p-3 flex justify-between items-center font-sans fixed top-0 left-0 w-full z-50 shadow-md">
     <div class="flex items-center w-full mx-auto">
       <a href="/" class="mr-0">
         <img
@@ -9,15 +9,16 @@
         />
       </a>
       <div class="flex flex-col ml-3 gap-1 cursor-pointer md:hidden" @click="toggleMenu">
-        <span v-for="index in 3" :key="index" class="w-6 h-1 bg-white rounded"></span>      </div>
+        <span v-for="index in 3" :key="index" class="w-6 h-1 bg-white rounded"></span>      
+      </div>
       <ul :class="[ 
         'list-none flex gap-1', 
         menuOpen ? 'flex flex-col absolute top-20 left-0 w-full bg-red-700 p-5' : 'hidden md:flex justify-start w-full'
       ]">
         <li v-for="item in navItems" :key="item.text">
-          <a :href="item.href" class="text-white font-semibold ml-2 py-2 px-4 rounded hover:bg-yellow-500 block">
+          <nuxt-link :to="item.href" class="text-white font-semibold ml-2 py-2 px-4 rounded hover:bg-yellow-500 block">
             {{ item.text }}
-          </a>
+          </nuxt-link>
         </li>
       </ul>
     </div>
@@ -30,14 +31,14 @@ import { ref } from 'vue'
 const navItems = [
   { href: '/', text: 'Home' },
   { href: '/players', text: 'Players' },
-  { href: '/matches', text: 'Matches' },
-  { href: '/news', text: 'News' },
-  { href: '/transfer', text: 'Transfer' },
-  { href: '/team', text: 'Team' },
-  { href: '/injury', text: 'Injury Report' },
-  { href: '/rating', text: 'Rating' },
-  { href: '/value', text: 'Player Value' },
-  { href: '/mvp', text: 'MVP' },
+  // { href: '/matches', text: 'Matches' },
+  // { href: '/news', text: 'News' },
+  // { href: '/transfer', text: 'Transfer' },
+  // { href: '/team', text: 'Team' },
+  // { href: '/injury', text: 'Injury Report' },
+  // { href: '/rating', text: 'Rating' },
+  // { href: '/value', text: 'Player Value' },
+  // { href: '/mvp', text: 'MVP' },
 ]
 
 const menuOpen = ref(false)
