7ce1a35008d186e3b0890e7f462d9e2dccfa131b
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-04-29 18:58:31 +0900

    add api for football api

---
+++ backend/app/services/football_api_service.rb
@@ -0,0 +1,15 @@
+class FootballApiService
+  
+  binding.pry
+  
+  include HTTParty
+  base_uri 'https://api.football-data.org/v4/teams/66'
+
+  def initialize
+    @headers = { "X-Auth-Token" => "b97dbadb4cbf43d2b11f223017d12731" }
+  end
+
+  def fetch_matches
+    self.class.get('/matches', headers: @headers)
+  end
+end
