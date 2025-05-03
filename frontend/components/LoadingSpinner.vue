de5292144fa09e8eec4f3e7a3574f7c6ab123957
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-05-03 23:08:19 +0900

    add components

---
+++ frontend/components/LoadingSpinner.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col items-center justify-center py-12">
+    <div class="inline-block animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-red-600"></div>
+    <p class="text-white mt-4 text-lg">{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  message: {
+    type: String,
+    default: 'Loading...'
+  }
+})
+</script>
