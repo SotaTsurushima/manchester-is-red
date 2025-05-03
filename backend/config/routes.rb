99ed821c8f61c2f2eda86ff793297fba720aee39
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-05-03 21:09:24 +0900

    add route for injuries

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/matches", to: "matches#index"
   get "/transfers", to: "transfers#index"
+  get '/injuries', to: 'injury_players#index'
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
