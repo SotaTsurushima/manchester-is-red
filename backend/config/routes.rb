35383e71f66416fec204c2b514c49b7ea0cce55f
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-05-04 23:45:40 +0900

    add route for uploading

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "/matches", to: "matches#index"
   get "/transfers", to: "transfers#index"
   get '/injuries', to: 'injury_players#index'
+  post 'upload', to: 'uploads#create'
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
