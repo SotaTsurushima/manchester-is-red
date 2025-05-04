954cb3ceceb325a56ab5c2ee0077da35cc6e92e5
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-05-04 23:46:03 +0900

    add upload page

--- frontend/components/Navigation.vue
+++ frontend/components/Navigation.vue
@@ -43,8 +43,8 @@ const navItems = [
   { href: '/players', text: 'Players' },
   { href: '/matches', text: 'Matches' },
   { href: '/transfers', text: 'Transfer' },
-  { href: '/injury', text: 'Injury Report' }
-  // { href: '/team', text: 'Team' },
+  { href: '/injury', text: 'Injury Report' },
+  { href: '/upload', text: 'Upload Images' }
   // { href: '/rating', text: 'Rating' },
   // { href: '/value', text: 'Player Value' },
   // { href: '/news', text: 'News' },
