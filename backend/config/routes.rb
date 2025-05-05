f7f3d38a45450967f5c33f1caf8c5aae8ea12aaa
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-05-05 12:42:12 +0900

    add route for players

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/transfers", to: "transfers#index"
   get '/injuries', to: 'injury_players#index'
   post 'upload', to: 'uploads#create'
+  resources :players, only: [:index, :create, :show, :update, :destroy]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
