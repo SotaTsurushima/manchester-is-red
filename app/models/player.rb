c21d52567fe4ec83ba842939a01545061c92d381
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-05-06 15:09:28 +0900

    delete unnecessary file

--- app/models/player.rb
+++
@@ -1,8 +0,0 @@
-class Player < ApplicationRecord
-  POSITIONS = %w[FW MF DF GK].freeze
-
-  validates :name, presence: true
-  validates :number, presence: true, numericality: { only_integer: true }
-  validates :image, presence: true
-  validates :position, presence: true, inclusion: { in: POSITIONS }
-end
