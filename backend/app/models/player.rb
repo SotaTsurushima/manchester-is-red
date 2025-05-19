eaf0741e07af8ba6ae530a1ef8a318523c311d0a
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-05-20 00:10:50 +0900

    add validation

--- backend/app/models/player.rb
+++ backend/app/models/player.rb
@@ -2,4 +2,6 @@ class Player < ApplicationRecord
   validates :name, presence: true
   validates :number, presence: true, numericality: { only_integer: true }
   validates :image, presence: true
+  validates :yellowcard, :redcard, :appearances, numericality: { greater_than_or_equal_to: 0 }
+  validates :market_value, numericality: { greater_than_or_equal_to: 0 }
 end
