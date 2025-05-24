4611ca1f74440ef5d6c35a67a936b7db8db5f326
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-05-24 14:37:16 +0900

    add market value

--- backend/app/workers/fetch_player_stats_worker.rb
+++ backend/app/workers/fetch_player_stats_worker.rb
@@ -1,10 +1,10 @@
-# app/workers/fetch_player_stats_worker.rb
 class FetchPlayerStatsWorker
   include Sidekiq::Worker
   sidekiq_options retry: 3  # 失敗時に3回までリトライ
 
   def perform
     stats = FbrefStatsService.new.fetch_player_stats
+    market_values = TransfermarktStatsService.new.fetch_market_values
   rescue => e
     Rails.logger.error "Error fetching player stats: #{e.message}"
     raise e  # Sidekiqのリトライ機能を利用
