e901724a865da41b72c51e97254026666ea089bc
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-05-25 16:12:37 +0900

    change directory

--- backend/app/controllers/matches_controller.rb
+++ backend/app/controllers/matches_controller.rb
@@ -1,7 +1,7 @@
 class MatchesController < ApplicationController
   def index
     competition_id = params[:competition_id] || 'PL'
-    matches = MatchesService.get_matches(competition_id)
+    matches = Matches::MatchesService.get_matches(competition_id)
     render json: { matches: matches }
   end
 end
