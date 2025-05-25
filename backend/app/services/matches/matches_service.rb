dc547d31cccb23a3aa898042fdd039693b306969
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-05-25 21:58:24 +0900

    simplified for db

--- backend/app/services/matches/matches_service.rb
+++ backend/app/services/matches/matches_service.rb
@@ -1,27 +1,21 @@
 module Matches
   class MatchesService
+    COMPETITION_MAP = {
+      'PL' => 'Premier League',
+      'UEL' => 'Europa Lg',
+      'FA' => 'FA Cup',
+      # ...他も同様
+    }
+
     def self.get_matches(competition_id)
-      Rails.cache.fetch("matches/#{competition_id}", expires_in: 5.minutes) do
-        new.fetch_and_format_matches(competition_id)
+      db_value = COMPETITION_MAP[competition_id] || competition_id
+      Rails.cache.fetch("matches/#{db_value}", expires_in: 5.minutes) do
+        matches = ::Match.where(competition: db_value).order(utc_date: :asc)
+        {
+          matches: matches.as_json,
+          competition: db_value
+        }
       end
-    end
-
-    def fetch_and_format_matches(competition_id)
-      matches_data = fetch_matches(competition_id)
-      format_response(matches_data, competition_id)
-    end
-
-    private
-
-    def fetch_matches(competition_id)
-      FootballApiService.new.get_matches(competition_id)
-    end
-
-    def format_response(matches_data, competition_id)
-      {
-        matches: matches_data[:data]['matches'],
-        competition: competition_id
-      }
     end
   end
 end
