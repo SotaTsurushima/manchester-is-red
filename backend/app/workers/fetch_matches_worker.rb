1821d6333d48a6fd292b9b8ebd8a6771d7c89d97
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-05-25 15:53:45 +0900

    add files for add matches table

---
+++ backend/app/workers/fetch_matches_worker.rb
@@ -0,0 +1,14 @@
+class FetchMatchesWorker
+  include Sidekiq::Worker
+  sidekiq_options retry: 3
+
+  def perform
+    begin
+      TransfermarktMatchesService.new.fetch_and_save_matches
+      Rails.logger.info 'Successfully fetched and saved matches from Transfermarkt.'
+    rescue => e
+      Rails.logger.error "Error fetching matches: #{e.message}"
+      raise e
+    end
+  end
+end 
