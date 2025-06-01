1ed8187989a156f6188b0a3daa4fc3b32f40c5b3
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-06-01 21:15:45 +0900

    add association

--- backend/app/models/match.rb
+++ backend/app/models/match.rb
@@ -2,4 +2,7 @@ class Match < ApplicationRecord
   validates :utc_date, :home_team, :away_team, :competition, presence: true
 
   scope :finished, -> { where(status: 'FINISHED') }
+
+  belongs_to :home_team, class_name: 'Team'
+  belongs_to :away_team, class_name: 'Team'
 end
