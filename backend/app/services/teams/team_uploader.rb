aea94f6ead8d8e5402164b316f4a341fab83c4a5
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-06-03 23:41:48 +0900

    modified for team photo

--- backend/app/services/teams/team_uploader.rb
+++ backend/app/services/teams/team_uploader.rb
@@ -1,11 +1,16 @@
+require 'open-uri'
+
 module Teams
   class TeamUploader
     include MinioSetting
 
     def self.upload_from_url(url, team_name)
       response = URI.open(url)
-      filename = "#{team_name.downcase.gsub(/\s+/, '_')}_crest.png"
+      # ファイル名を安全に
+      safe_name = team_name.downcase.gsub(/\s+/, '_').gsub(/[^a-z0-9_]/, '')
+      filename = "#{safe_name}_crest.png"
       key = "teams/#{filename}"
+
       obj = s3_resource.bucket(BUCKET).object(key)
       obj.put(body: response.read, content_type: 'image/png')
       base_url = ENDPOINT.sub('minio', 'localhost')
