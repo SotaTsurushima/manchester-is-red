45632d3467c06f793585c6bdd60bb8fd5beb9b8e
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-06-08 00:43:47 +0900

    add show and before action

--- backend/app/controllers/matches_controller.rb
+++ backend/app/controllers/matches_controller.rb
@@ -1,4 +1,6 @@
 class MatchesController < ApplicationController
+  before_action :set_match, only: [:show]
+
   def index
     competition_id = params[:competition_id] || 'PL'
     page = params[:page].to_i > 0 ? params[:page].to_i : 1
@@ -6,5 +8,15 @@ class MatchesController < ApplicationController
 
     matches = Matches::MatchesService.get_matches(competition_id, page: page, per_page: per_page)
     render json: { matches: matches }
+  end
+
+  def show
+    render json: { match: @match }
+  end
+
+  private
+
+  def set_match
+    @match = Match.find(params[:id])
   end
 end
