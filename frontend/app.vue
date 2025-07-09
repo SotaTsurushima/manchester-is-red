ef45d32197f741429eb315c8ea76a401feff1ae1
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-07-09 17:52:41 +0900

    not displaying navigation when it's in admin site

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-    <Navigation />
+    <!-- 管理画面以外でナビゲーションを表示 -->
+    <Navigation v-if="!isAdminPage" />
     <NuxtPage />
   </div>
 </template>
 
 <script setup>
-import Navigation from '~/components/Navigation.vue';
+import Navigation from '~/components/Navigation.vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+// 管理画面かどうかを判定
+const isAdminPage = route.path.startsWith('/admin')
 </script>
