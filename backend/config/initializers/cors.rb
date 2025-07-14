7a49bd8780bc9386e3defae05d8eefbec20f0eb7
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-07-14 23:33:04 +0900

    modified cors for token

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -14,6 +14,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: %i[get post put patch delete options head],
       credentials: true,
-      expose: ['Authorization']
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client']
   end
 end
