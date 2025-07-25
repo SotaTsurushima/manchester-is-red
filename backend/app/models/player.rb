e8768f3a12f13e9023afbe475fed94bc94ae329b
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-07-26 00:45:55 +0900

    remove validations for yellow_card, red_card, appearances, market_value, and salary in Player model

--- backend/app/models/player.rb
+++ backend/app/models/player.rb
@@ -2,7 +2,4 @@ class Player < ApplicationRecord
   validates :name, presence: true
   validates :number, presence: true, numericality: { only_integer: true }
   validates :image, presence: true
-  validates :yellow_card, :red_card, :appearances, numericality: { greater_than_or_equal_to: 0 }
-  validates :market_value, numericality: { greater_than_or_equal_to: 0 }
-  validates :salary, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 end
