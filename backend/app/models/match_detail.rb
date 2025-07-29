12ef2fb7a20773cade13d7ad47077546d27f50ca
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-07-29 18:34:39 +0900

    remove commented validation and scope sections in MatchDetail model for cleaner code

--- backend/app/models/match_detail.rb
+++ backend/app/models/match_detail.rb
@@ -1,11 +1,9 @@
 class MatchDetail < ApplicationRecord
   belongs_to :match
   
-  # バリデーション
   validates :scorer_name, presence: true, if: :goal_exists?
   validates :assist_name, presence: true, if: :assist_exists?
   
-  # スコープ
   scope :goals, -> { where.not(scorer_name: nil) }
   scope :assists, -> { where.not(assist_name: nil) }
   
