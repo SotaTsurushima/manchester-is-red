43f3ce82cab33b89bcf2103735964a97619cd9e7
sotatsurushima <2018cassotatsurushima@gmail.com>
2025-07-29 19:10:20 +0900

    add association for match detail

--- backend/app/models/match.rb
+++ backend/app/models/match.rb
@@ -5,4 +5,9 @@ class Match < ApplicationRecord
 
   belongs_to :home_team, class_name: 'Team'
   belongs_to :away_team, class_name: 'Team'
+  
+  has_many :match_details, dependent: :destroy
+  
+  has_many :goals, -> { where.not(scorer_name: nil) }, class_name: 'MatchDetail'
+  has_many :assists, -> { where.not(assist_name: nil) }, class_name: 'MatchDetail'
 end
