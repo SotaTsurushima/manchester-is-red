905832082871b5ee51823ee95c56c081f35e275f
SotaTsurushima <2018cassotatsurushima@gmail.com>
2025-10-12 20:39:50 +0900

    modified file name

--- backend/app/workers/fetch_player_stats_worker.rb
+++
@@ -1,9 +0,0 @@
-class FetchPlayerStatsWorker
-  include Sidekiq::Worker
-  sidekiq_options retry: 3  # 失敗時に3回までリトライ
-
-  def perform
-    result = PlayerBatch::FetchFbrefData.call
-    result.fail!(error: 'FBrefデータの取得に失敗しました') unless result.success?
-  end
-end
